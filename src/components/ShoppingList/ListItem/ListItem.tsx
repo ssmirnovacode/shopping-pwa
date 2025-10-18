e80d74f20b2227e2c824067077151866a8a8d9a0
ssmirnovacode <svetlana.smirnova213@gmail.com>
2025-10-18 15:10:41 +0200

    ListItem styles with grid

--- src/components/ShoppingList/ListItem/ListItem.tsx
+++ src/components/ShoppingList/ListItem/ListItem.tsx
@@ -1,3 +1,4 @@
+import { useState, type ChangeEvent, type InputEvent } from "react";
 import type { ShoppingListItem } from "../../../utils/types";
 import "./ListItem.scss";
 
@@ -6,11 +7,37 @@ type Props = {
 };
 
 export const ListItem = ({ item }: Props) => {
+  const [values, setValues] = useState({
+    price: item.price,
+    quantity: item.quantity,
+  });
+
+  const handlePriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValues((values) => ({ ...values, price: Number(e.target.value) }));
+  };
+
+  const handleQuantityChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValues((values) => ({ ...values, quantity: Number(e.target.value) }));
+  };
   return (
     <li className="listItem">
       <div className="name">{item.name}</div>
-      <div className="quantity">{item.quantity}</div>
-      <div className="price">{item.price}</div>
+      <div className="quantity">
+        <input
+          id="input-quantity"
+          type="number"
+          onChange={handleQuantityChange}
+          value={values.quantity}
+        />
+      </div>
+      <div className="price">
+        <input
+          id="input-price"
+          type="number"
+          onChange={handlePriceChange}
+          value={values.price}
+        />
+      </div>
       <button className="add">Add</button>
       <button className="delete">Dlt</button>
     </li>
